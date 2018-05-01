0f22864e8741e2fda68d3b2e3a0d9ad87bba2be2
briecodes <hello@brie.work>
2018-05-01 17:05:25 -0400

    basic associations completed.

--- app/controllers/songs_controller.rb
+++ app/controllers/songs_controller.rb
@@ -9,12 +9,22 @@ class SongsController < ApplicationController
 
   def new
     @song = Song.new
+    @genres = Genre.all
   end
 
   def create
     @song = Song.new(song_params)
+    if !params[:song][:artist].empty?
+      @song.artist = Artist.find_or_create_by(name: params[:song][:artist])
+    end
 
     if @song.save
+      if !params[:notes][:content][0].empty?
+        params[:notes][:content].each do |c|
+          no = Note.create(content: c, song_id: @song.id)
+        end
+      end
+
       redirect_to @song
     else
       render :new
@@ -47,7 +57,7 @@ class SongsController < ApplicationController
   private
 
   def song_params
-    params.require(:song).permit(:title)
+    params.require(:song).permit(:title, :genre_id)
   end
 end
 
--- app/models/song.rb
+++ app/models/song.rb
@@ -2,5 +2,40 @@ class Song < ActiveRecord::Base
   belongs_to :artist
   belongs_to :genre
   has_many :notes
-end
 
+  def note_contents
+    if self.notes
+      self.notes.map{|c| c.content }
+    end
+  end
+
+  def genre_name=(name)
+    self.genre = Genre.find_by name: name
+  end
+
+  def genre_name
+    self.genre.name
+  end
+
+  def artist_name=(name)
+    # byebug
+    self.artist = Artist.find_or_create_by(name: name)
+  end
+
+  def artist_name
+    self.artist.name
+  end
+
+  def note_contents=(content)
+    content.each do |con|
+      if !con.empty?
+        self.notes << Note.create(content: con)
+      end
+    end
+  end
+
+  def note_contents
+    self.notes.map {|c| c.content }
+  end
+
+end
