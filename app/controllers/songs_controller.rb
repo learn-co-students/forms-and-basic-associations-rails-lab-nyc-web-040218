6e9e77dcc2b14b0895f584f5ef79557617912b58
github username <github email address>
2018-05-02 14:44:55 -0400

    a few changes

--- app/controllers/songs_controller.rb
+++ app/controllers/songs_controller.rb
@@ -1,3 +1,4 @@
+require 'byebug'
 class SongsController < ApplicationController
   def index
     @songs = Song.all
@@ -9,10 +10,22 @@ class SongsController < ApplicationController
 
   def new
     @song = Song.new
+    @genres = Genre.all
   end
 
   def create
     @song = Song.new(song_params)
+
+    unless params[:song][:artist].empty?
+      @artist = Artist.find_or_create_by(name: params[:song][:artist])
+      @song.artist_id = @artist.id
+    end
+
+    unless params[:notes][:content][0].empty?
+      @note1 = Note.create(content: params[:notes][:content][0])
+      @note2 = Note.create(content: params[:notes][:content][1])
+      @song.notes = [@note1, @note2]
+    end
 
     if @song.save
       redirect_to @song
@@ -40,14 +53,13 @@ class SongsController < ApplicationController
   def destroy
     @song = Song.find(params[:id])
     @song.destroy
-    flash[:notice] = "Song deleted."
+    flash[:notice] = 'Song deleted.'
     redirect_to songs_path
   end
 
   private
 
   def song_params
-    params.require(:song).permit(:title)
+    params.require(:song).permit(:title, :genre_id)
   end
 end
-
--- app/models/song.rb
+++ app/models/song.rb
@@ -2,5 +2,33 @@ class Song < ActiveRecord::Base
   belongs_to :artist
   belongs_to :genre
   has_many :notes
-end
 
+  def genre_name=(name)
+    self.genre = Genre.find_or_create_by(name: name)
+  end
+
+  def genre_name
+      self.genre.name
+   end
+
+  def artist_name=(name)
+    self.artist = Artist.find_or_create_by(name: name)
+  end
+
+  def artist_name
+    self.artist.name
+  end
+
+  def note_contents=(content)
+   content.each do |e|
+     if !e.empty?
+       self.notes << Note.create(content: e)
+     end
+   end
+ end
+
+ def note_contents
+   self.notes.map {|e| e.content }
+ end
+
+end
